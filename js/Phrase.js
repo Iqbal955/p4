edd967cb096b83928c074533fb69a5ea6008c2b6
Iqbal955 <58709666+Iqbal955@users.noreply.github.com>
2020-04-10 08:32:01 +0200

    Update Phrase.js

--- js/Phrase.js
+++ js/Phrase.js
@@ -6,13 +6,40 @@ class Phrase {
 
     constructor(phrase) {
 
-        this.phrase = phrase.toLowercase;
+        this.phrase = phrase.toLowerCase();
         console.log("test");
 
 
     }
 
 
+    addPhraseToDisplay() {
 
+        var splitT = this.phrases.split(""); //Calling the split on the phrases
+
+        var letters = splitT.forEach(letter => {
+
+            if (letter.value == ! " ") {
+                var liLetter = '<li>' + letter + '</li>';
+                letter.setAttribute("class", "hide letter");
+                document.getElementById("phrase").firstElementChild(letter).appendChild(liLetter);
+
+
+
+            }
+
+            else {
+
+                var liSpaces = '<li>' + letter + '</li>';
+                letter.setAttribute("class", "hide spaces");
+                document.getElementById("phrase").firstElementChild(letter).appendChild(liSpaces);
+
+            }
+
+
+
+        });
+
+    }
 
 }
