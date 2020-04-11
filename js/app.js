676f3a0a6fdf75aabf6f83b1d0c90c1f773752a0
Iqbal955 <58709666+Iqbal955@users.noreply.github.com>
2020-04-11 08:36:33 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,12 +2,58 @@
  * Project 4 - OOP Game App
  * app.js */
 
-const phrase = new Phrase('Life is like a box of chocolates');
+
+/**
+ * Testing the new objects
+ * */
+//const phrase = new Phrase('Life is like a box of chocolates');
 //console.log(`Phrase - phrase: ${phrase.phrase}`);
 
 
+//const game = new Game();
+//game.possiblePhrases.forEach((possiblePhrases, index) => {
+  //  console.log(`Phrase ${index} - phrase: ${possiblePhrases}`);
+//});
+
+//const game = new Game();
+//game.possiblePhrases.forEach((possiblePhrases, index) => {
+ //console.log(`Phrase ${index} - phrase: ${possiblePhrases}`);
+//});
+
+
+//var nextPhrase = new Phrase(game.possiblePhrases[0]);
+//console.log(nextPhrase);
+
+
+const getPhrases = (possiblePhrases) => {
+    console.log("Possible phrase" + possiblePhrases);
+}
 const game = new Game();
-game.phrases.forEach((phrase, index) => {
-    console.log(`Phrase ${index} - phrase: ${phrase.phrase}`);
+getPhrases(game.getRandomPhrase);
+getPhrases(game.getRandomPhrase);
+getPhrases(game.getRandomPhrase);
+
+
+
+const phrase = new Phrase('Life is like a box of chocolates');
+console.log(`Phrase - phrase: ${phrase.phrase}`);
+
+game.getRandomPhrase();
+
+/** 
+const game;
+document.getElementById("btn_reset").addEventListener("click", function() {
+
+    game = new Game;
+    game.startGame();
+
+
+
+
+
+
+
+
 });
+*/
 
