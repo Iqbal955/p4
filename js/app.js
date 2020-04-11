c558d915623b64bb9d1540cbd2d681f85620010c
Iqbal955 <58709666+Iqbal955@users.noreply.github.com>
2020-04-11 20:19:06 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -15,53 +15,55 @@
   //  console.log(`Phrase ${index} - phrase: ${possiblePhrases}`);
 //});
 
+
 const game = new Game();
 const randomPhrase = game.getRandomPhrase();
-const phrase = new Phrase(randomPhrase.possiblePhrases);
+const phrase = new Phrase(randomPhrase.chosenPhrase);
 phrase.addPhraseToDisplay();
 
 
-//const getPhrases = (possiblePhrases) => {
-  //  console.log("Possible phrase" + possiblePhrases);
-//}
-//const game = new Game();
-//getPhrases(game.getRandomPhrase);
-//getPhrases(game.getRandomPhrase);
-//getPhrases(game.getRandomPhrase);
+
+/*
+const getPhrases = (possiblePhrases) => {
+    console.log("Possible phrase " + possiblePhrases);
+    }
+    const game = new Game();
+    getPhrases(game.getRandomPhrase());
+    getPhrases(game.getRandomPhrase());
+    getPhrases(game.getRandomPhrase());
 
 
 
-//const phrase = new Phrase('Life is like a box of chocolates');
-console.log(`Phrase - phrase: ${phrase.phrase}`);
+    //const phrase = new Phrase('Life is like a box of chocolates');
+    //console.log(`Phrase - phrase: ${phrase.phrase}`);
 
 
-//const game = new Game();
-//game.possiblePhrases.forEach((possiblePhrases, index) => {
- //console.log(`Phrase ${index} - phrase: ${possiblePhrases}`);
-//});
+    //const game = new Game();
+    //game.possiblePhrases.forEach((possiblePhrases, index) => {
+    //console.log(`Phrase ${index} - phrase: ${possiblePhrases}`);
+    //});
 
 
-//var nextPhrase = new Phrase(game.possiblePhrases[0]);
-//console.log(nextPhrase);
+    //var nextPhrase = new Phrase(game.possiblePhrases[0]);
+    //console.log(nextPhrase);
 
 
-game.getRandomPhrase();
+    //game.getRandomPhrase();
 
-/** 
-const game;
-document.getElementById("btn_reset").addEventListener("click", function() {
-
-    game = new Game;
-    game.startGame();
-
-
-
-
-
-
-
-
-});
-*/
-
-
+    /** 
+    const game;
+    document.getElementById("btn_reset").addEventListener("click", function() {
+    
+        game = new Game;
+        game.startGame();
+    
+    
+    
+    
+    
+    
+    
+    
+    });
+    
+}*/
