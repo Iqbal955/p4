d623114184940fa9bb5622e3f00fbdb8489e9c66
Iqbal955 <58709666+Iqbal955@users.noreply.github.com>
2020-04-11 08:33:29 +0200

    Update Game.js

--- js/Game.js
+++ js/Game.js
@@ -7,18 +7,40 @@ class Game {
 
 
         this.missed = 0;
-        this.phrases = ["ABC", "DEF", "GameTest", "NewGame", "Cannon"];
+        this.possiblePhrases = ["ABC", "DEF", "GameTest", "NewGame", "Cannon"];
         this.activePhrase = null;
       
+    }
 
 
+    getRandomPhrase() {
+
+     this.possiblePhrases[Math.floor(Math.random() * this.possiblePhrases.length)];
+     
+    }
+    /** 
+
+    startGame() {
+
+        const overLay = document.getElementById("overlay");
+        overLay.style.display = "none";
+      const activePhrase =  addPhraseToDisplay(getRandomPhrase());
 
 
     }
 
+
+
+
+    handleInteraction() {
+
+        checkLetter()
+
+    }
+
+
   
+  */
 
+    }
 
-
-
-}
