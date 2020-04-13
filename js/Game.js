fca8075eb3c375ed242f9278ae7cf9a8e7b051fe
Iqbal955 <58709666+Iqbal955@users.noreply.github.com>
2020-04-13 08:14:22 +0200

    Update Game.js

--- js/Game.js
+++ js/Game.js
@@ -22,29 +22,34 @@ class Game {
  return this.possiblePhrases[Math.floor(Math.random() * this.possiblePhrases.length)];
  
     }
-    /** 
+    
 
     startGame() {
 
-        const overLay = document.getElementById("overlay");
+        const overLay = document.getElementById("overlay"); 
         overLay.style.display = "none";
-      const activePhrase =  addPhraseToDisplay(getRandomPhrase());
 
+        this.activePhrase = this.getRandomPhrase(); //Setting the this.getRandomPhrase to the varibale
+
+      /*  
+        const addRandomPhraseToDisplay = addPhraseToDisplay(getRandomPhrase); //creating a nested function //Error here addPhraseToDisplay is not defined
+        this.activePhrase.addRandomPhraseToDisplay; //setting the activePhraseParameter to the addRandomPhrase
+        */
 
     }
 
-
+    
 
 
     handleInteraction() {
 
-        checkLetter()
+        checkLetter();
 
     }
 
 
   
-  */
+ 
 
     }
 
