4be5e930d72d30ab2813a2cce26dbd1286aef937
Iqbal955 <58709666+Iqbal955@users.noreply.github.com>
2020-04-19 11:28:19 +0200

    Update Phrase.js

--- js/Phrase.js
+++ js/Phrase.js
@@ -9,7 +9,78 @@ class Phrase {
 
     }
 
+    addPhraseToDisplay() {
+
+
+        const phraseID = document.getElementById("phrase");
+        var splitPhrase = this.actualPhrase.split("");
+        splitPhrase.forEach(letter => {
+
+            if (letter !== " ") {
+
+                var liLetter = document.createElement("li");
+                liLetter.textContent = letter;
+                liLetter.setAttribute("class", "hide letter");
+                phraseID.firstElementChild.appendChild(liLetter);
+
+
+
+            }
+
+
+            else {
+
+
+                var liSpace = document.createElement("li");
+                liSpace.textContent = letter;
+                liSpace.setAttribute("class", "space");
+                phraseID.firstElementChild.appendChild(liSpace);
+
+
+
+            }
+
+
+        });
+
+    }
+
+
+    checkLetter(letter) {
+        return this.actualPhrase.includes(letter);
+
+    }
+
+
+    showMatchedLetter(letter) {
+
+        const allLetters = document.querySelectorAll(".letter");
+
+
+        allLetters.forEach(HTMLLetter => {
+
+            if (HTMLLetter.innerHTML === letter) {
+
+                HTMLLetter.classList.replace("hide", "show");
+
+
+
+            }
+
+
+
+        });
+
+
+
+    }
+
 
 
 }
+
+
+
+
+
 
