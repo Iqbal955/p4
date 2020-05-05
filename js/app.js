fdede41c22b952d65ec300c17fc34e458e784861
Iqbal955 <58709666+Iqbal955@users.noreply.github.com>
2020-05-05 22:13:16 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -11,8 +11,6 @@
 
 const game = new Game(); //creates a new game object and sets it to game
 
-
-function newGame() { //new game function (to be used later)
     
     const reset = document.getElementById("btn__reset") //event listener
     reset.addEventListener("click", e => {
@@ -40,66 +38,8 @@ function newGame() { //new game function (to be used later)
         });
 
 
-        if (game.gameOver) { //if game.gameOver is true, the restartGame callback is called.
-
-
-            restartGame()
-
-        };
-
+       
     });
 
 
-            }
-
-function restartGame() { 
-
-
-    
-
-   //the restart game callback function resets everything, once it has reset all it call the previous function, new Game
-
-        var buttons = document.querySelectorAll("button");
-        buttons.forEach(button => button.disabled = false);
-
-        var phraseButtons = document.getElementById("phrase").firstElementChild;
-        phraseButtons.remove();
-
-
-
-        var wrong = document.getElementsByClassName("wrong");
-        for (var i = 0; i < wrong.length; i++) {
-            wrong[i].classList.remove("wrong");
-        }
-
-
-
-        var chosen = document.getElementsByClassName("chosen");
-        for (var i = 0; i < chosen.length; i++) {
-            chosen[i].classList.remove("chosen");
-        }
-
-
-        //removing the li elements by looping through the phrase dom elements, getting the firstElementChild (ul). 
-        //removing the child of it(with the phrase[i])
-
-        var scoreboard = document.querySelectorAll("img");
-    scoreboard.forEach((heart) => (heart.src = "images/liveHeart.png"));
-
-
-
-
-        newGame();
-
-
-
-
-
-
-
-    };
-
-
-newGame() //the newGame function is being called intially
-
-    //}
+          
