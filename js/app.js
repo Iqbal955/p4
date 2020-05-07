7f5767f3ca4c78ea2a3479373e691642258e408b
Iqbal955 <58709666+Iqbal955@users.noreply.github.com>
2020-05-07 16:33:50 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -6,36 +6,20 @@
 
 //Reset button event listener
 
+let game;
 
 
-
-const game = new Game(); //creates a new game object and sets it to game
 
     
     const reset = document.getElementById("btn__reset") //event listener
-    reset.addEventListener("click", e => {
+reset.addEventListener("click", e => {
 
+        game = new Game(); //creates a new game object and sets it to game
+
+        game.restartGame();
 
         game.startGame(); //starts game
 
-
-        //sets the scoreboard for each item to live heart.
-        var scoreboard = document.querySelectorAll("img");
-        scoreboard.forEach((heart) => (heart.src = "images/liveHeart.png"));
-
-
-        //Keyboard eventlistener
-        const lettersonKeyboard = document.getElementById("qwerty").addEventListener("click", (e) => {
-
-
-            //Check condition, if clicked event has tag name button, execute handleInteraction()
-            if (event.target.tagName === 'BUTTON') {
-                game.handleInteraction(event.target);
-            };
-
-
-
-        });
 
 
        
